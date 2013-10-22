dc116d52514ee9a9caf5b5bafa16cd1f38aa95b1
Mario <Mario@Mario-PC>
2013-10-22 20:35:26 +0200

    “Versió 0.2 del projecte prjava01

--- prjava01/src/prjava01/Prjava01.java
+++ prjava01/src/prjava01/Prjava01.java
@@ -1,33 +1,38 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-package prjava01;
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prjava01;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 /**
- *
- * @author daw2_m08uf4pr2
- */
-import java.awt.Desktop;
-import java.io.*;
-public class Prjava01 {
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) throws IOException {
-        File f = new File("fitxer.html");
-        try (BufferedWriter bw = new BufferedWriter(new FileWriter(f))) {
-            bw.write("<html>");bw.newLine();
-            bw.write("  <head>");bw.newLine();
-            bw.write("    <title>");bw.newLine();
-            bw.write("      Nova p&agrave;gina  web");bw.newLine();
-            bw.write("    </title>");bw.newLine();
-            bw.write("  </head>");bw.newLine();
-            bw.write("  <body>");bw.newLine();
-            bw.write("    <h1>DAW2 m08uf4pr2</h1>");bw.newLine();
-            bw.write("    Nova p&agrave;gina web");bw.newLine();
-            bw.write("  </body>");bw.newLine();
-            bw.write("</html>");bw.newLine();
-            bw.close();
-        }
-    }
+ *
+ * @author enrique
+ */
+public class Prjava01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+        File f = new File("fitxer.html");
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(f))) {
+            bw.write("<html>");bw.newLine();
+            bw.write("  <head>");bw.newLine();
+            bw.write("    <title>");bw.newLine();
+            bw.write("      Nova p&agrave;gina  web");bw.newLine();
+            bw.write("    </title>");bw.newLine();
+            bw.write("  </head>");bw.newLine();
+            bw.write("  <body>");bw.newLine();
+            bw.write("    <h1>DAW2 m08uf4pr2</h1>");bw.newLine();
+            bw.write("    Nova p&agrave;gina web");bw.newLine();
+            bw.write("  </body>");bw.newLine();
+            bw.write("</html>");bw.newLine();
+            bw.close();
+        }
+    }
 }
